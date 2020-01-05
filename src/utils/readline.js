f066a855c8f94c4d54b18f16ff17323eefcea92d
GEPANG <2455124772@qq.com>
2020-01-05 15:49:08 +0800

    分析日志

---
+++ src/utils/readline.js
@@ -0,0 +1,31 @@
+
+
+const fs=require('fs');
+const path=require('path');
+const readline=require('readline');
+
+const fileName=path.join(__dirname,'../','../','logs','access.log');
+
+const readStream=fs.createReadStream(fileName);
+
+const rl=readline.createInterface({
+    input:readStream
+});
+
+let chromeNum=0;
+let sum=0;
+rl.on('line',(lineDate)=>{
+    if(!lineDate){
+        return;
+    }
+    sum++;
+
+    const arr=lineDate.split(' -- ');
+    if(arr[2] && arr[2].indexOf('Firefox')>0){
+        chromeNum++;
+    }
+   
+})
+rl.on('close',()=>{
+    console.log('chrome占比是：',chromeNum/sum*100);
+})
